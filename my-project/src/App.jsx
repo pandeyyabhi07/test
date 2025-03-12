27677fb27e8734e69a86b5b061008a5bf7411f18
Pandeyyabhi07 <pandeyyabhi001@gmail.com>
2025-03-12 13:19:06 +0530

    done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -12,7 +12,7 @@ const usersData = [
 
 const UserCard = ({ name, email, age }) => {
   return (
-    <div className="bg-black w-full text-white shadow-lg rounded-2xl p-6 border border-gray-700">
+    <div className="bg-black w-1/2 text-white shadow-lg rounded-2xl p-6 border border-gray-700">
       <p className="text-lg font-bold">Name:</p>
       <p className="mb-2">{name}</p>
       <p className="text-lg font-bold">Age:</p>
@@ -26,7 +26,7 @@ const UserCard = ({ name, email, age }) => {
 const Dashboard = () => {
   const [users] = useState(usersData);
   return (
-    <div className="flex flex-wrap justify-center gap-4 p-6">
+    <div className="flex bg-slate-400 flex-wrap justify-center gap-4 p-6">
       {users.map((user, index) => (
         <UserCard key={index} {...user} />
       ))}
@@ -35,14 +35,14 @@ const Dashboard = () => {
 };
 
 const Home = () => (
-  <div className="text-center text-2xl font-semibold p-10 w-full  bg-gray-300">Welcome to User Dashboards</div>
+  <div className="text-center text-2xl font-semibold p-10 w-full h-screen flex items-center justify-center  bg-gray-300">Welcome to User Dashboards</div>
 );
 
 const App = () => {
   return (
     <Router>
-      <div className="p-6">
-        <nav className="flex flex-wrap justify-center space-x-6 mb-6">
+      <div className="bg-blue-500">
+        <nav className="flex flex-wrap justify-center items-center space-x-6 mb-6 py-4">
           <Link className="text-white bg-black px-4 py-2 rounded" to="/">Home</Link>
           <Link className="text-white bg-black px-4 py-2 rounded" to="/dashboard">Dashboard</Link>
         </nav>
